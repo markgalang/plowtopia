0f1843c34261f37fb070a9f3d40d381196c1abf8
Mark Galang <galangmarkg@gmail.com>
2021-09-03 20:05:01 +0800

    Add About Section

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,37 @@
 <template>
   <div id="app">
-    <Navbar />
-    <Hero />
-    <About />
-    <GameEconomy />
+    <!-- <Navbar />
+    <Hero /> -->
+    <Container>
+      <About />
+    </Container>
+    <!-- <GameEconomy />
     <RoadMap />
-    <Footer />
+    <Footer /> -->
   </div>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar/Navbar";
-import Footer from "@/components/Footer/Footer";
-import Hero from "@/layout/Hero/Hero";
+// import Navbar from "@/components/Navbar/Navbar";
+// import Footer from "@/components/Footer/Footer";
+// import Hero from "@/layout/Hero/Hero";
+import Container from "@/components/Container/Container";
 import About from "@/layout/About/About";
-import RoadMap from "@/layout/Roadmap/Roadmap";
-import GameEconomy from "@/layout/GameEconomy/GameEconomy";
+// import RoadMap from "@/layout/Roadmap/Roadmap";
+// import GameEconomy from "@/layout/GameEconomy/GameEconomy";
 
 export default {
   name: "App",
   components: {
-    Hero,
-    Navbar,
+    Container,
+    // Hero,
+    // Navbar,
     About,
-    GameEconomy,
-    RoadMap,
-    Footer,
+    // GameEconomy,
+    // RoadMap,
+    // Footer,
   },
 };
 </script>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-</style>
+<style src="./App.css"></style>
---
+++ src/components/Container/Container.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: "Container",
+};
+</script>
+
+<style scoped src="./Container.css"></style>
