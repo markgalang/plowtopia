d0ccf266c2b7b6ca34ec981c31e6c2fa8f89e91a
Mark Galang <galangmarkg@gmail.com>
2021-09-14 19:52:07 +0800

    Update navbar links

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -18,29 +18,40 @@
               class="navbar-link"
               >Whitepaper</a
             >
-            <a href="#roadmap" class=" navbar-link">Cultivator</a>
+            <a
+              :href="cultivatorLink"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="navbar-link"
+              >Cultivator</a
+            >
             <a href="#roadmap" class=" navbar-link">Play</a>
           </scrollactive>
         </div>
-        <img @click="toggle()" :src="Menu" alt="Menu" class="menu" />
+        <img @click="toggleMenu()" :src="Menu" alt="Menu" class="menu" />
         <a
-          href="https://google.com"
+          :href="getTokenLink"
           class="getToken"
           target="_blank"
           rel="noopener noreferrer"
           >Get a Token</a
         >
-        <div class="menuMobile" :class="{ showMenu: state }">
+        <div class="menuMobile" :class="{ showMenu: isMenuVisible }">
           <div class="menuContentMobile">
             <div class="closeButtonContainer">
-              <img @click="toggle()" :src="Close" alt="Close" class="close" />
+              <img
+                @click="toggleMenu()"
+                :src="Close"
+                alt="Close"
+                class="close"
+              />
             </div>
 
             <div class="menuLinksContainer">
               <ul>
-                <li><a href="" class="active">Home</a></li>
-                <li><a href="">About Plowtopia</a></li>
-                <li><a href="">Game Roadmap</a></li>
+                <li><a href="#home" class="active">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Roadmap</a></li>
                 <li><a href="">White Paper</a></li>
               </ul>
             </div>
@@ -54,7 +65,6 @@
 import PlowTopia from "@/assets/plowtopia-logo.png";
 import Menu from "@/assets/menu.svg";
 import Close from "@/assets/close.svg";
-// import Whitepaper from "@/assets/whitepaper.pdf";
 
 export default {
   name: "Navbar",
@@ -62,13 +72,14 @@ export default {
     PlowTopia,
     Menu,
     Close,
-    // Whitepaper,
-    state: false,
+    isMenuVisible: false,
+    cultivatorLink: "https://app.plowtopia.io",
+    getTokenLink: "https://pancakeswap.com",
   }),
 
   methods: {
-    toggle() {
-      this.state = !this.state;
+    toggleMenu() {
+      this.isMenuVisible = !this.isMenuVisible;
     },
   },
 };
