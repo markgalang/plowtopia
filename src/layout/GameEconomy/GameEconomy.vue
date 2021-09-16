b408b4e8b44790272f84bcf4bd247de7eb408408
Mark Galang <galangmarkg@gmail.com>
2021-09-16 18:13:51 +0800

    Update icons

--- src/layout/GameEconomy/GameEconomy.vue
+++ src/layout/GameEconomy/GameEconomy.vue
@@ -11,6 +11,15 @@
       <div class="economyContainer">
         <div class="economy">
           <img :src="MarketIcon" alt="Market Economy" class="economyImage" />
+          <h2 class="economyTitle">NFT Marketplace</h2>
+          <p class="economyText">
+            The main currency on PlowTopia game and Web Marketplace is PLOT.
+            Users can purchase numerous Items and NFTs using the token where it
+            creates a strong demand.
+          </p>
+        </div>
+        <div class="economy">
+          <img :src="CoinIcon" alt="Play to Earn" class="economyImage" />
           <h2 class="economyTitle">Play to Earn</h2>
           <p class="economyText">
             Gems are the in-game currency earned by farming and virtual trading.
@@ -21,22 +30,12 @@
         </div>
 
         <div class="economy">
-          <img :src="MarketIcon" alt="Market Economy" class="economyImage" />
+          <img :src="GemIcon" alt="Staking Rewards" class="economyImage" />
           <h2 class="economyTitle">Staking Rewards</h2>
           <p class="economyText">
             A Staking platform will be deployed that will incentivized stakers
             and Liquidity providers whereas holders will gain a share from
             protocol fees and revenue which will be paid out in $PLOT.
-          </p>
-        </div>
-
-        <div class="economy">
-          <img :src="MarketIcon" alt="Market Economy" class="economyImage" />
-          <h2 class="economyTitle">NFT Marketplace</h2>
-          <p class="economyText">
-            The main currency on PlowTopia game and Web Marketplace is PLOT.
-            Users can purchase numerous Items and NFTs using the token where it
-            creates a strong demand.
           </p>
         </div>
       </div>
@@ -46,16 +45,16 @@
 <script>
 import GameEconomyLabel from "@/assets/game-economy-label.svg";
 import MarketIcon from "@/assets/market.svg";
-// import CoinIcon from "@/assets/coin.svg";
-// import GemIcon from "@/assets/gem.svg";
+import CoinIcon from "@/assets/coin.svg";
+import GemIcon from "@/assets/gem.svg";
 
 export default {
   name: "GameEconomy",
   data: () => ({
     GameEconomyLabel,
     MarketIcon,
-    // CoinIcon,
-    // GemIcon,
+    CoinIcon,
+    GemIcon,
   }),
 };
 </script>
